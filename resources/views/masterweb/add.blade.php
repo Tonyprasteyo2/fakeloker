7c382e1872598276a75b1a7850733900257d0ac9
Tonyprasteyo2 <tonyprasetyo251@gmail.com>
2022-11-27 08:03:19 -0800

    lupa validasinya

--- resources/views/masterweb/add.blade.php
+++ resources/views/masterweb/add.blade.php
@@ -90,7 +90,7 @@
                         <div class="row mb-3">
                             <div class="col-12 col-lg-6">
                                 <label for="namapt" class="form-label">Nama Perusaaan</label>
-                                <input type="text" class="form-control" id="namapt" placeholder="Masukan nama perusaaan" name="namapt"/>
+                                <input type="text" class="form-control" id="namapt" placeholder="Masukan nama perusaaan" name="namapt" required/>
                             </div>
                             <div class="col col-lg-6">
                                 <label for="statuspt" class="form-label">Status Kebenaran</label>
