f5156681d777f591840d54a98aa5ad982df06aa1
Tonyprasteyo2 <tonyprasetyo251@gmail.com>
2022-12-02 07:27:10 -0800

    menu

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -2,7 +2,7 @@
     <ul id="sidebarnav">
         <!-- User Profile-->
         <li class="sidebar-item pt-2">
-            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="masterweb"
+            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('masterweb') }}"
                 aria-expanded="false">
                 <i class="far fa-clock" aria-hidden="true"></i>
                 <span class="hide-menu">Dashboard</span>
@@ -10,39 +10,41 @@
         </li>
         <li class="sidebar-item">
             <div class="dropdown dropend">
-                <a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-toggle" href="profile.html"
+                <a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-toggle"
                 aria-expanded="false" data-bs-toggle="dropdown" id="menu-informasi">
                     <i class="fa fa-info-circle" ></i>
                     <span class="hide-menu">Informasi</span>
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="menu-informasi">
-                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="informasi"><i class="fa fa-info-circle"></i>
+                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="{{ route('informasi') }}"><i class="fa fa-info-circle"></i>
                         <span>Lihat Informasi</span>
                         </a>
                     </li>
-                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="add"><i class="fa fa-plus"></i>
+                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="{{ route('add') }}"><i class="fa fa-plus"></i>
                         <span>Tambah Informasi</span>
                         </a>
                     </li>
                 </ul>
             </div>
         </li>
-        <li class="sidebar-item">
+
+        {{-- <li class="sidebar-item">
             <a class="sidebar-link waves-effect waves-dark sidebar-link" href="user"
                 aria-expanded="false">
                 <i class="fa fa-file-archive" aria-hidden="true"></i>
                 <span class="hide-menu">Data User</span>
             </a>
-        </li>
+        </li> --}}
+
         <li class="sidebar-item">
             <div class="dropdown dropend">
-                <a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-toggle" href="profile.html"
+                <a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-toggle"
                 aria-expanded="false" data-bs-toggle="dropdown" id="menu-informasi">
                     <i class="fa fa-cog" aria-hidden="true"></i>
                     <span class="hide-menu">Setting</span>
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="menu-informasi">
-                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="profil"><i class="fa fa-user"></i>
+                    <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link dropdown-item" href="{{ route('profil')}}"><i class="fa fa-user"></i>
                         <span>Profil</span>
                         </a>
                     </li>
